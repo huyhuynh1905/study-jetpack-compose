60f2a47134081684ed28c1d10c1ba9293b925457
huyhuynh1905 <huyhuynh1905@gmail.com>
2023-08-29 23:53:43 +0700

    [C] clean code not use import

--- app/src/main/java/com/huyhuynh/studycompose/ButtonActivity.kt
+++ app/src/main/java/com/huyhuynh/studycompose/ButtonActivity.kt
@@ -9,9 +9,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
@@ -19,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.huyhuynh.studycompose.ui.theme.StudyComposeTheme
